40d9dac86e337addd71b5ac4639d6f998e2f0f57
Developer LuisF3 <developerluisfernando3@gmail.com>
2022-05-19 22:25:47 -0500

    [MOD, miniDrawer.tsx][ADD, auth]

--- components/navbar/miniDrawer.tsx
+++ components/navbar/miniDrawer.tsx
@@ -1,17 +1,44 @@
 // React Js
 import * as React from "react";
 
+// Next Js
+import Link from "next/link";
+import Image from "next/image";
+
+// next-auth
+import { useSession, signOut } from "next-auth/react";
+
+// Material React JS
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
+
 // Material Ui
-import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import {
+  styled,
+  alpha,
+  useTheme,
+  Theme,
+  CSSObject,
+} from "@mui/material/styles";
+// Components
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
+// Icons
 import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
@@ -97,6 +124,45 @@ const Drawer = styled(MuiDrawer, {
   }),
 }));
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
 interface Props {
   children: React.ReactNode;
 }
@@ -104,7 +170,17 @@ interface Props {
 // Component
 let MiniDrawer = ({ children }: Props) => {
   const theme = useTheme();
+  const { data: session, status } = useSession();
   const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -114,12 +190,145 @@ let MiniDrawer = ({ children }: Props) => {
     setOpen(false);
   };
 
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      {status === "authenticated" ? (
+        <a
+          href={`/api/auth/signout`}
+          onClick={(e) => {
+            e.preventDefault();
+            signOut();
+          }}
+        >
+          <MenuItem>
+            <Typography variant="body1" color="primary">
+              Logout
+            </Typography>
+          </MenuItem>
+        </a>
+      ) : (
+        <Link href="/login">
+          <a>
+            <MenuItem>
+              <Typography color="primary">Login</Typography>
+            </MenuItem>
+          </a>
+        </Link>
+      )}
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      color="default"
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      {status === "authenticated" ? (
+        <a
+          href={`/api/auth/signout`}
+          onClick={(e) => {
+            e.preventDefault();
+            signOut();
+          }}
+        >
+          <MenuItem>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="primary-search-account-menu"
+              aria-haspopup="true"
+              color="primary"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Typography variant="body1" color="primary">
+              Logout
+            </Typography>
+          </MenuItem>
+        </a>
+      ) : (
+        <Link href="/login">
+          <a>
+            <MenuItem>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="primary-search-account-menu"
+                aria-haspopup="true"
+                color="primary"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Typography variant="body1" color="primary">
+                Login
+              </Typography>
+            </MenuItem>
+          </a>
+        </Link>
+      )}
+    </Menu>
+  );
+
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ flexGrow: 1, display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar>
           <IconButton
+            color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
@@ -127,11 +336,107 @@ let MiniDrawer = ({ children }: Props) => {
               marginRight: 5,
               ...(open && { display: "none" }),
             }}
-            color="inherit"
           >
             <MenuIcon />
           </IconButton>
-          <Typography color="inherit">Next App Example</Typography>
+          <Typography
+            color="inherit"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Next App Example
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Tooltip title="open settings">
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              color="inherit"
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+          {status === "loading" && (
+            <Box sx={{ flexGrow: 0, ml: 2 }}>
+              <Stack sx={{ color: "grey.500" }} spacing={2} direction="row">
+                <CircularProgress color="success" />
+              </Stack>
+            </Box>
+          )}
+          {status === "authenticated" && (
+            <Box sx={{ flexGrow: 0, ml: 2 }}>
+              <Tooltip title="open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar>
+                    <Image
+                      src={session.user.image}
+                      alt="My Logo"
+                      width="100%"
+                      height="100%"
+                    />
+                  </Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <Typography variant="h6" color="primary">
+                  {session.user.name}
+                </Typography>
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography color="primary" textAlign="center">
+                    Account
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          )}
+          {status === "unauthenticated" && (
+            <Box sx={{ flexGrow: 0, ml: 2 }}>
+              <Avatar alt="MyAvatar" src="/img/insta.png" />
+            </Box>
+          )}
         </Toolbar>
       </AppBar>
       <Drawer
@@ -151,6 +456,8 @@ let MiniDrawer = ({ children }: Props) => {
         <Divider color="divider" />
         <List />
       </Drawer>
+      {renderMobileMenu}
+      {renderMenu}
       <DrawerHeader />
       <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
         <div>{children}</div>
